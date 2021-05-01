5dd77d7ca23cc3d5ada0606e3435461097416b85
xajik <xajik@users.noreply.github.com>
2021-05-01 09:34:26 +0000

    deploy: 4f4741cc021b407b87759c262bcd0f0820d56fb3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,8 +14,8 @@ const RESOURCES = {
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"index.html": "f6b4ae84b433b1631f93cd9b2660ccb4",
-"/": "f6b4ae84b433b1631f93cd9b2660ccb4"
+"index.html": "465eb62a8be52cac8202f3d6a349e6a8",
+"/": "465eb62a8be52cac8202f3d6a349e6a8"
 };
 
 // The application shell files that are downloaded before a service worker can
