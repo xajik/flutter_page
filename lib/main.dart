866dc914c716a10f453d3e19277e4aad714a060c
Igor Steblii <igor.stebliy@gmail.com>
2021-05-01 17:26:37 +0800

    Update typo

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Web Page deployed with GitLab Actions'),
+      home: MyHomePage(title: 'Flutter Web Page deployed with GitHub Actions'),
     );
   }
 }
